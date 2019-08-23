3adfb160727db7a7e79d95b2163f8ef80d69fe27
Madeline Starr <40212869+ariadne27@users.noreply.github.com>
2019-08-23 14:25:37 -0400

    pushing updates

--- static/js/app.js
+++ static/js/app.js
@@ -8,43 +8,7 @@ var tbody = d3.select("tbody");
 // Console.log the weather data from data.js
 console.log(tableData);
 
-// // Step 1: Loop Through `data` and console.log each weather report object
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-// });
 
-// // Step 2:  Use d3 to append one table row `tr` for each weather report object
-// // Don't worry about adding cells or text yet, just try appending the `tr` elements.
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-// });
-
-// // Step 3:  Use `Object.entries` to console.log each weather report value
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//   });
-// });
-
-// // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-// data.forEach(function(weatherReport) {
-//   console.log(weatherReport);
-//   var row = tbody.append("tr");
-
-//   Object.entries(weatherReport).forEach(function([key, value]) {
-//     console.log(key, value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     var cell = row.append("td");
-//   });
-// });
-
-// // Step 5: Use d3 to update each cell's text with
-// weather report values (weekday, date, high, low)
 data.forEach(function(tableData) {
   console.log(tableData);
   var row = tbody.append("tr");
@@ -68,7 +32,7 @@ function dateFilter() {
   
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
+      td = tr[i].getElementsByTagName("td")[3];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.indexOf(filter) > -1) {
@@ -91,7 +55,7 @@ function cityFilter() {
   
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[1];
+      td = tr[i].getElementsByTagName("td")[0];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.indexOf(filter) > -1) {
